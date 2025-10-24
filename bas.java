6a7fb0e2b45a43bd00af04f79f125d596005407f
Utkarsh Pandey <ankurpandey1813@gmail.com>
2025-10-24 21:40:28 +0530

    Add files via upload

---
+++ bas.java
@@ -0,0 +1,15 @@
+//arguments
+class bas{
+public static void main(String [] st){
+System.out.println("Hello Utkarsh");
+System.out.println(st[0]);
+
+}
+}
+/*
+C:\Users\ankur\OneDrive\Desktop\Javanote>javac bas.java
+
+C:\Users\ankur\OneDrive\Desktop\Javanote>java bas Utkarsh
+Hello Utkarsh
+Utkarsh
+*/
